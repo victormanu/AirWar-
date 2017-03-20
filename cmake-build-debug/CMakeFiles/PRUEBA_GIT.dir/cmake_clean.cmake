bbf9b53ad96c6d4e4752b6217c7b5a6ec3e641e6
Victork <Montoneros1>
2017-03-19 23:33:20 -0600

    mi primer commit

---
+++ cmake-build-debug/CMakeFiles/PRUEBA_GIT.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PRUEBA_GIT.dir/main.cpp.o"
+  "PRUEBA_GIT.pdb"
+  "PRUEBA_GIT"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PRUEBA_GIT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
